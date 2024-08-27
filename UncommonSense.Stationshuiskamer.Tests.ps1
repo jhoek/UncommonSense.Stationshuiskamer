78f53cd1087cb2f60e48a21cc8b209d0eceb14b0
Jan Hoek <github@uncommonsense.nl>
2024-08-27 13:46:18 +0200

    Preparations

---
+++ UncommonSense.Stationshuiskamer.Tests.ps1
@@ -0,0 +1,27 @@
+Describe UncommonSense.Stationshuiskamer {
+    BeforeAll {
+        Import-Module "$PSScriptRoot/UncommonSense.Stationshuiskamer.psd1" -Force
+        $Result = Get-Stationshuiskamer
+    }
+
+    It 'Returns at least 12 entries' {
+        $Result.Length | Should -BeGreaterOrEqual 12
+    }
+
+    It 'Returns valid names' {
+        $Result.Name | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Returns valid addresses' {
+        $Result.Address | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Returns valid images' {
+        $Result.Image | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Returns valid opening hours' {
+        $Result.OpeningHours.Length | Should -Be 7
+        $Result.OpeningHours.
+    }
+}
